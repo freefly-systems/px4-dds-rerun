0b4cf34a9b0a4dd590f38e5c491aa004fbfcbb35
Peter Breuer <peter.breuer.profiles@gmail.com>
2025-03-03 00:34:54 +0000

    Add launch config; change qos settings

--- src/px4_ros_rerun/px4_ros_rerun/px4_ros_rerun.py
+++ src/px4_ros_rerun/px4_ros_rerun/px4_ros_rerun.py
@@ -18,7 +18,7 @@ class PX4RerunNode(Node):
         # Configure QoS profile for subscribing
         qos_profile = QoSProfile(
             reliability=ReliabilityPolicy.BEST_EFFORT,
-            durability=DurabilityPolicy.TRANSIENT_LOCAL,
+            durability=DurabilityPolicy.VOLATILE,
             history=HistoryPolicy.KEEP_LAST,
             depth=1,
         )
@@ -40,11 +40,47 @@ class PX4RerunNode(Node):
         web_port = int(os.getenv("RR_WEBVIEWER_PORT", 9090))
         rr.serve_web(web_port=web_port)
 
-    def local_pos_callback(self, msg):
-        pass
+        # Local NED frame
+        rr.log("world", rr.ViewCoordinates.RIGHT_HAND_Z_DOWN, static=True)
+        rr.log(
+            "world/earth-ned",
+            rr.Arrows3D(
+                vectors=[[1, 0, 0], [0, 1, 0], [0, 0, -1]],
+                colors=[[255, 0, 0], [0, 255, 0], [0, 0, 255]],
+            ),
+            static=True,
+        )
 
-    def att_callback(self, msg):
-        print(msg.q)
+        # Ground plane
+        rr.log(
+            "world/ground",
+            rr.Boxes3D(
+                mins=[-100, -100, -1],
+                sizes=[200, 200, 1],
+                colors=[255, 255, 255],
+                fill_mode="solid",
+            ),
+            static=True,
+        )
+
+        self.last_callback_time = self.get_clock().now()
+
+    def local_pos_callback(self, msg: VehicleLocalPosition):
+        if self.limit_fps():
+            return
+        rr.log("world/body-frd", rr.components.Translation3D(xyz=[msg.x, msg.y, msg.z]))
+
+    def att_callback(self, msg: VehicleAttitude):
+        if self.limit_fps():
+            return
+        rr.log("world/body-frd", rr.RotationQuat(xyzw=list(msg.q)))
+
+    def limit_fps(self, fps: int = 60) -> bool:
+        now = self.get_clock().now()
+        if (now - self.last_callback_time).nanoseconds / 1e9 < (1.0 / fps):
+            return 1
+        self.last_callback_time = now
+        return 0
 
 
 def main(args=None):
